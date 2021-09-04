0d79060baacbf6d790ed2137a853ad9bf936bd82
Tulikasharma <tulikasharma368@gmail.com>
2021-09-04 16:52:33 +0530

    UC4

--- empWageArrray.js
+++ empWageArrray.js
@@ -3,6 +3,7 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HRS = 4;
 const FULL_TIME_HRS = 8;
 const WAGE_PER_HR = 20;
+const NUM_OF_WORKING_DAYS = 20;
 
 function getEmpWorkingHrs(empCheck) {
     switch(empCheck) {
@@ -16,8 +17,11 @@ function getEmpWorkingHrs(empCheck) {
                 return 0;
     }
 }
-let empHrs = 0;
-let empCheck = Math.floor(Math.random() * 10) % 3;
-empHrs = getEmpWorkingHrs(empCheck);
-let empWage = empHrs * WAGE_PER_HR;
-console.log("Employee wage: "+empWage);
+
+let totalEmpHrs = 0;
+for (day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    totalEmpHrs += getEmpWorkingHrs(empCheck);
+}
+let empWage = totalEmpHrs * WAGE_PER_HR;
+console.log("Employee Total Working Hours: "+totalEmpHrs +" Employee wage: "+empWage);
