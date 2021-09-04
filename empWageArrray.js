9c495bfebef7544265b52bc7c8d12366b4d8a9f6
Tulikasharma <tulikasharma368@gmail.com>
2021-09-04 16:51:17 +0530

    UC3

--- empWageArrray.js
+++ empWageArrray.js
@@ -1,24 +1,23 @@
-  
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HRS = 4;
 const FULL_TIME_HRS = 8;
 const WAGE_PER_HR = 20;
 
-let empHrs = 0;
-
-let empCheck = Math.floor(Math.random() * 10) % 3;
-switch(empCheck) {
-    case IS_PART_TIME:
-        empHrs = PART_TIME_HRS;
-        break;
+function getEmpWorkingHrs(empCheck) {
+    switch(empCheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HRS;
     
-    case IS_FULL_TIME:
-        empHrs = FULL_TIME_HRS;
-        break;
+        case IS_FULL_TIME:
+            return FULL_TIME_HRS;
     
-        default:
-            empHrs = 0;
+            default:
+                return 0;
+    }
 }
+let empHrs = 0;
+let empCheck = Math.floor(Math.random() * 10) % 3;
+empHrs = getEmpWorkingHrs(empCheck);
 let empWage = empHrs * WAGE_PER_HR;
 console.log("Employee wage: "+empWage);
