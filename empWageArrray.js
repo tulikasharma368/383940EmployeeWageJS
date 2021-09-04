6f18e1635775cbbd24d9149764e37757166e8c32
Tulikasharma <tulikasharma368@gmail.com>
2021-09-04 16:58:58 +0530

    UC6

--- empWageArrray.js
+++ empWageArrray.js
@@ -1,31 +1,44 @@
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
-const PART_TIME_HRS = 4;
-const FULL_TIME_HRS = 8;
-const WAGE_PER_HR = 20;
-const NUM_OF_WORKING_DAYS = 20;
-const MAX_HRS_IN_MONTH = 160;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+const NUMBER_OF_WORKING_DAYS = 20;
+const  MAX_HOURS_IN_MONTH = 160;
 
-function getEmpWorkingHrs(empCheck) {
-    switch(empCheck) {
-        case IS_PART_TIME:
-            return PART_TIME_HRS;
-    
-        case IS_FULL_TIME:
-            return FULL_TIME_HRS;
-    
-            default:
-                return 0;
+function getWorkingHours(empCheck){
+    switch(empCheck){
+    case IS_FULL_TIME:
+        return FULL_TIME_HOURS;
+    case IS_PART_TIME:
+        return PART_TIME_HOURS;
+    default:
+        return 0;
     }
 }
 
-let totalEmpHrs = 0;
-let totalWorkingDays = 0;
-while (totalEmpHrs <= MAX_HRS_IN_MONTH &&
-        totalWorkingDays < NUM_OF_WORKING_DAYS) {
-            totalWorkingDays++;
-            let empCheck = Math.floor(Math.random() * 10) % 3;
-            totalEmpHrs += getEmpWorkingHrs(empCheck);
+function calcDailyWage(empHrs){
+    return empHrs * WAGE_PER_HOUR;
 }
-let empWage = totalEmpHrs * WAGE_PER_HR;
-console.log("Total Working Days: "+totalWorkingDays +" Employee Total Working Hours: "+totalEmpHrs +" Employee wage: "+empWage);
+
+let totalEmpHours = 0;
+let totalWorkingDays = 0;
+let empDailyWageArr = new Array();
+
+while(totalEmpHours <= MAX_HOURS_IN_MONTH &&
+      totalWorkingDays <= NUMBER_OF_WORKING_DAYS){
+      totalWorkingDays++;  
+      let empCheck = Math.floor(Math.random() * 10);
+      let empHrs = getWorkingHours(empCheck);
+      totalEmpHours += empHrs;
+      empDailyWageArr.push(calcDailyWage(empHrs));
+        }
+
+let  empWage = totalEmpHours * WAGE_PER_HOUR;
+console.log("UC 6: " + "\n" +
+            "Total days: " + totalWorkingDays + "\n" +
+            "Total Hours: " + totalEmpHours + "\n" +
+            "Emp Wage: " + empWage);
+console.log("Daily wage array: " + empDailyWageArr);
+
+
