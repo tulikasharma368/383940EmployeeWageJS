c33f044ec0de7a33fa5759b09132f6d2af0d9d21
Tulikasharma <tulikasharma368@gmail.com>
2021-09-06 21:46:17 +0530

    UC 12: Refractor class

--- employeePayrollData.js
+++ employeePayrollData.js
@@ -1,19 +1,26 @@
 class EmployeePayrollData{
     id;
-    name;
     salary;
+    gender;
+    startDate;
 
-    constructor(id, name, salary){
-        this.id = id;
-        //this.name = name;
-        this.salary = salary;
+    constructor(...params){
+        this.id = params[0];
+        this.name = params[1];
+        this.salary = params[2];
+        this.gender = params[3];
+        this.startDate = params[4];
     }
 
-    get name(){return this.name}    
-    set name(name){this.name = name;}
+    get name(){return this._name}    
+    set name(name){this._name = name;}
 
     toString(){
-       return "Id:" + this.id + ", Name: " + this.name + ", Salary: "+ this.salary;
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const empDate = this.startDate === undefined ? "undefined" : this.startDate.toLocaleDateString("en-US", options);
+
+
+       return "Id:" + this.id + ", Name: " + this.name + ", Salary: "+ this.salary + ", Gender: " + this.gender + ", Start date: " + empDate;
     }
 }
 
@@ -21,5 +28,6 @@ let employeePayrollData = new EmployeePayrollData(1, "Mark", 9000000);
 console.log(employeePayrollData.toString());
 employeePayrollData.name = "John";
 console.log(employeePayrollData.toString());
-
+employeePayrollData = new EmployeePayrollData(2, "Terisa", 10000000, "F", new Date());
+console.log(employeePayrollData.toString());
 
